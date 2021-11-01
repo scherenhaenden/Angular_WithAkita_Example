d0f387eb2d52d197e2409befa611f9105df1f86b
Eddie Krill <scherenhaenden@gmail.com>
2021-11-01 14:19:36 +0100

    Proto Project updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { SessionStore } from './store/session-store';
+import { SessionQuery } from './store/session.query';
+// ESM
+import {parse, stringify, toJSON, fromJSON} from 'flatted';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularReduxCrudExampleV2';
+  name$ = this.query.select('name');
+
+  public myModel= 'anything'
+
+  constructor(
+    private store: SessionStore,
+    private query: SessionQuery
+  ) {
+
+    this.myModel = query.selectName;
+    console.log('name')
+
+  }
+
+  public saverange(): void {
+    console.log(this.myModel);
+
+    this.store.update(({name}) => ({name: this.myModel}));
+
+    const url = new URL(window.location.href);
+
+
+
+    //history.pushState(stringify(this.store), '', window.location.href)
+
+  }
+
+
+
 }
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+
+import { persistState } from '@datorama/akita';
+
+const storage = persistState();
+
+const providers = [{ provide: 'persistStorage', useValue: storage }];
+
 if (environment.production) {
   enableProdMode();
 }
